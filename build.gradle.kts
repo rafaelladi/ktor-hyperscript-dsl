39209866304cc6d45ee9c75f553821a4c49813ed
rafaeldietrich-fl <rafael.dietrich@fountainledge.com>
2023-10-17 15:42:17 -0300

    remove unnecessary dependency. update dsl builder function name. fix error.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.9.1")
-    implementation("io.ktor:ktor-server-core-jvm:2.3.5")
 
     testImplementation(kotlin("test"))
 }
--- src/main/kotlin/com/dietrich/HyperscriptDSL.kt
+++ src/main/kotlin/com/dietrich/HyperscriptDSL.kt
@@ -1,16 +1,10 @@
 package com.dietrich
 
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 import kotlinx.html.HTMLTag
-import kotlinx.html.TagConsumer
-import kotlinx.html.h1
-import kotlinx.html.stream.appendHTML
-
-interface HyperScriptDSL {
-    fun hyperScript(): String
-}
+import kotlin.collections.joinToString
+import kotlin.collections.mutableListOf
+import kotlin.collections.plusAssign
+import kotlin.collections.set
 
 enum class Trigger(val value: String) {
     CLICK("click")
@@ -28,33 +22,13 @@ class HyperScriptBuilder {
     }
 
     fun toggle(action: String) {
-        commands += "toggle .red"
+        commands += "toggle $action"
     }
 
     fun build(): String = commands.joinToString(" ")
 }
 
-fun HTMLTag.hyperScript(builder: HyperScriptBuilder = HyperScriptBuilder(), block: HyperScriptBuilder.() -> Unit) {
+fun HTMLTag.hyperscript(builder: HyperScriptBuilder = HyperScriptBuilder(), block: HyperScriptBuilder.() -> Unit) {
     val hyperScript = builder.apply(block).build()
     attributes["_"] = hyperScript
-}
-
-suspend inline fun ApplicationCall.respondHtmlFragment(crossinline block: TagConsumer<StringBuilder>.() -> Unit) = respondText {
-    buildString { appendHTML().apply(block) }
-}
-
-fun Application.routing() {
-    routing {
-        get {
-            call.respondHtmlFragment {
-                h1 {
-                    hyperScript {
-                        on(Trigger.CLICK)
-                        toggle(".red")
-                        on("me")
-                    }
-                }
-            }
-        }
-    }
 }
